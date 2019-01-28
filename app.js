b6588a0cf18c99b7a95b543b07c011b039956e04
Cruz Ortiz <cortiz99>
2019-01-27 20:00:38 -0400

    testing object

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 var greet = require('./modules/greet.js');
 greet();
+
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastname = lastName;
+}
+Person.prototype.greet = function () {
+  console.log('Hello, ' + this.firstName + ' ' + this.lastname);
+}
+var john = new Person('John', 'Doe');
+console.log(john.greet());
