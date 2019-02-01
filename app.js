f695dcd0dccf9e7315b2c30bd6d80d7e8eec4b09
Cruz Ortiz <cortiz99>
2019-01-31 20:54:48 -0400

    builded event emitter

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-var greet = require('./modules/greet');
-var spanish = require('./modules/greet').spanish;
-greet.spanish();
-greet.english();
-spanish();
+var Emitter = require('./modules/emitter')
+
+var emitter = new Emitter();
+
+emitter.on('greet', function () {
+  console.log('Somewhere, someone said hello');
+});
+
+emitter.on('greet', function () {
+  console.log('A greeting accurred !');
+})
+
+emitter.emit('greet');
