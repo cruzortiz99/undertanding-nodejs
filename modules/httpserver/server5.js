533d2203aa34172558f7882618945e16d9f25a75
Cruz Ortiz <cortiz99>
2019-02-11 22:21:13 -0400

    routing

--- modules/httpserver/server5.js
+++ modules/httpserver/server5.js
@@ -16,8 +16,7 @@ http.createServer(function (req, res) {
         res.end(chunk)
       }
     })
-  }
-  if (req.url === '/api') {
+  } else if (req.url === '/api') {
     res.writeHead(200, {
       'Content-Type': 'application/json'
     })
@@ -26,5 +25,8 @@ http.createServer(function (req, res) {
       lastName: 'Ortiz Castañeda'
     }
     res.end(JSON.stringify(obj))
+  } else {
+    res.writeHead(404)
+    res.end()
   }
 }).listen(8080, '127.0.0.1')
