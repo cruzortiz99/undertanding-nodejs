41318f787f17448ae9a168be5fc28fd87cf807ca
Cruz Ortiz <cortiz99>
2019-02-11 21:41:45 -0400

    building a template with streams

--- modules/httpserver/server3.js
+++ modules/httpserver/server3.js
@@ -6,10 +6,19 @@ var server = http.createServer(function (req, res) {
     'Content-Type': 'text/html'
   });
   var path = __dirname + '/index.html';
+  const highWaterMark = 64 * 1024;
   // Using pipes to improve performance
   // Use pipes whenever you can to build fastest app
   // Build Customs streams to implement templates
-  fs.createReadStream(path).pipe(res);
+  let html = fs.createReadStream(path, { encoding: 'utf8', highWaterMark: highWaterMark })
+  html.on('data', (chunk) => {
+    chunk = chunk.replace('{Message}', 'Cruz Ortiz')
+    if (chunk.length >= highWaterMark) {
+      res.write(chunk)
+    } else {
+      res.end(chunk)
+    }
+  })
 }).listen(8080, '127.0.0.1');
 
 module.exports = server
